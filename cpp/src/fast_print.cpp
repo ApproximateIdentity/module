9ea3e904badd8b2ac107da9039b4ba3d1cc18992
Thomas Nyberg <tomnyberg@gmail.com>
2016-09-30 12:10:09 -0400

    Add lock to library for thread-saftey

--- cpp/src/fast_print.cpp
+++ cpp/src/fast_print.cpp
@@ -3,14 +3,19 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
+#include <mutex>
 
 
 using namespace std;
 
+mutex gil;
+
 void fast_print_strings(vector<string> strings) {
     this_thread::sleep_for(chrono::milliseconds(500));
+    gil.lock();
     for (const auto& s : strings) {
         cout << s << "\t";
     }
     cout << endl;
+    gil.unlock();
 }
